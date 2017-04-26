d0893a7520198f4a41917efd4d61e68ede3307e2
eilarraza <erick@ubiot.io>
2017-04-26 10:26:27 +0200

    version inicial capitulo 3

---
+++ Chapter3.lisp
@@ -0,0 +1,22 @@
+(defvar *db* nil)
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defun add-record (cd) (push cd *db*))
+
+(defun dump-db()
+  (dolist(cd *db*)
+    (format t "~{a:~10t~a~%~}~%" cd)))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+    (prompt-read "Title")
+    (prompt-read "Artist")
+    (or parse-integer (prompt-read "Rating") :junk-allowed t)
+    (prompt-read "Ripped [y/n]")))
